783dd62454518abefb0d5e8cf6c3f3a09f542e1f
keerthiga.v <keerthigavasudevan2007@gmail.com>
2026-07-17 18:37:25 +0530

    add html application

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    prediction = ""
+
+    if request.method == "POST":
+        study_hours = request.form["studyhours"]
+        attendance = request.form["attendance"]
+
+        prediction = f"Study Hours: {study_hours}, Attendance: {attendance}"
+
+    return render_template("index.html", prediction=prediction)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
