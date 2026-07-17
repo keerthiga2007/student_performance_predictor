0f1204a31b694c76d1d50854d4f9055160ee2139
keerthiga.v <keerthigavasudevan2007@gmail.com>
2026-07-17 17:42:32 +0530

    add HTML application

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Welcome to the Home Page!"
-if __name__ == '__main__':
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
